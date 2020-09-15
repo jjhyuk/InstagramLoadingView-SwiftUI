af6622e6c2bbfc4e567c74a2eaf993bec271df1f
jinhyuk <jjhyuk15@gmail.com>
2020-09-15 21:08:07 +0900

    Initial Commit

---
+++ InstaLoadingView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaLoadingView
+//
+//  Created by jinhyuk on 2020/09/15.
+//  Copyright © 2020 jinhyuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
